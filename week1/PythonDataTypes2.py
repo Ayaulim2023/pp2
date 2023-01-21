d2d9aebca0b9b1d9401747b16d5806297492e570
Ayaulim2023 <ayaulim1002k@gmail.com>
2023-01-21 19:49:58 +0600

    Setting the Specific Data Type

---
+++ week1/PythonDataTypes2.py
@@ -0,0 +1,60 @@
+x = int(5)
+print(x)
+print(type(x))
+
+x1 = str("Hello World")
+print(x1)
+print(type(x1))
+
+x2 = int(20)
+print(x2)
+print(type(x2))
+
+x3 = float(20.5)
+print(x3)
+print(type(x3))
+
+x4 = complex(1j)
+print(x4)
+print(type(x4))
+
+x5 = list(("apple", "banana", "cherry"))
+print(x5)
+print(type(x5))
+
+x6 = tuple(("apple", "banana", "cherry"))
+print(x6)
+print(type(x6))
+
+x7 = range(6)
+print(x7)
+print(type(x7))
+
+x8 = dict(name="John", age=36)
+print(x8)
+print(type(x8))
+
+x9 = set(("apple", "banana", "cherry"))
+print(x9)
+print(type(x9))
+
+x10 = frozenset(("apple", "banana", "cherry"))
+print(x10)
+print(type(x10))
+
+x11 = bool(5)
+print(x11)
+print(type(x11))
+
+x12 = bytes(5)
+print(x12)
+print(type(x12))
+
+x13 = bytearray(5)
+print(x13)
+print(type(x13))
+
+x14 = memoryview(bytes(5))
+print(x14)
+print(type(x14))
+
