ab03b90597c43a2da4a64275d9ac4d877b195f4f
Ayaulim2023 <ayaulim1002k@gmail.com>
2023-01-25 20:26:26 +0600

    part 1

---
+++ week1/PythonOperators.py
@@ -0,0 +1,26 @@
+#Python Arithmetic Operators
+
+x = 5
+y = 3
+
+print(x + y)
+
+print(x - y)
+
+print(x * y)
+
+z = 12
+v = 3
+print(z / v)
+
+print(x % y)
+
+print(x ** y) #same as 5*5*5
+
+n = 15
+m = 2
+
+print(n // m)
+
+#the floor division // rounds the result down to the nearest whole number
+
