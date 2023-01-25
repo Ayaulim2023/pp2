eebd0611fb8d75b48a6d357324a4bb5cf55edbcb
Ayaulim2023 <ayaulim1002k@gmail.com>
2023-01-25 20:34:43 +0600

    part 1

---
+++ week1/PythonOperators2.py
@@ -0,0 +1,54 @@
+x = 5
+print(x)
+
+x += 3
+print(x)
+
+x -= 3
+print(x)
+
+x *= 3
+print(x)
+
+x /= 3
+print(x)
+
+x%=3
+print(x)
+
+y = 5
+
+y//=3
+print(y)
+
+y **= 3
+
+print(y)
+
+z = 5
+
+z &= 3
+
+print(z)
+
+x1 = 5
+
+x1 |= 3
+print(x1)
+
+x2 = 5
+
+x2 ^= 3
+print(x2)
+
+x3 = 5
+
+x3 >>= 3
+print(x3)
+
+x4 = 5
+
+x4 <<= 3
+print(x4)
+
+
