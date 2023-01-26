07e00dcc968cbfdb5c6b7c131c3d6dbcd9b63201
Ayaulim2023 <ayaulim1002k@gmail.com>
2023-01-26 12:39:24 +0600

    part 1

---
+++ week1/StringsExercize.py
@@ -0,0 +1,34 @@
+#first
+x = "Hello World"
+print(len(x))
+
+#second
+txt = "Hello World"
+x = txt[0]
+
+#third
+txt = "Hello World"
+x = txt[2:5]
+
+#fourth
+txt = " Hello World "
+x = txt.strip()
+
+#fifth
+txt = "Hello World"
+txt = txt.upper()
+
+#sixth
+txt = "Hello World"
+txt = txt.lower()
+
+#seventh
+txt = "Hello World"
+txt = txt.replace("H","J")
+
+#eight
+age = 36
+txt = "My name is John, and I am {}"
+print(txt.format(age))
+
+
