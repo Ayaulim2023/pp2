87fb1616c8aea592f1e1fc945dc9fec4ea7d7faf
Ayaulim2023 <ayaulim1002k@gmail.com>
2023-01-26 11:42:12 +0600

    part 1

---
+++ week1/PythonTutor1.5.py
@@ -0,0 +1,8 @@
+n = str(input())
+m = int(n)+1
+k = str(m)
+x=int(n)-1
+y=str(x)
+print("The next number for the number " + n + " is "+k+".")
+
+print("The previous number for the number " + n + " is " +y+"." )
