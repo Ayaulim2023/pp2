909ddd918beec9f67b88efccc655776b4584581b
Ayaulim2023 <ayaulim1002k@gmail.com>
2023-01-26 12:26:22 +0600

    part 1

---
+++ week1/VariablesExercize.py
@@ -0,0 +1,28 @@
+#first
+
+carname = "Volvo"
+
+#second
+x = 50
+
+#third
+x = 5
+y = 10
+print(x+y)
+
+#fourth
+x = 5
+y = 10
+z = x+y
+print(z)
+
+#fifth
+myfirst_name = "John"
+
+#sixth
+x = y = z = "Orange"
+
+#seventh
+def myfunc():
+  global x
+  x = "fantastic"
