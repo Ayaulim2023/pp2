164a53f0c71e5b7cbfaf1143e77b33cb94fdce76
Ayaulim2023 <ayaulim1002k@gmail.com>
2023-01-26 12:46:09 +0600

    part 1

---
+++ week1/OperatorsExercize.py
@@ -0,0 +1,18 @@
+#first
+print(10*5)
+
+#second
+print(10/2)
+
+#third
+fruits = ["apple", "banana"]
+if "apple" in fruits:
+  print("Yes, apple is a fruit!")
+
+#fourth
+if 5 != 10:
+  print("5 and 10 is not equal")
+
+#fifth
+if 5 == 10 or 4 == 4:
+  print("At least one of the statements is true")
