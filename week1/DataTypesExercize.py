2bb1daeb5ddf7f0b77ab69c5f322a8747b9dcf68
Ayaulim2023 <ayaulim1002k@gmail.com>
2023-01-26 12:30:30 +0600

    part 1

---
+++ week1/DataTypesExercize.py
@@ -0,0 +1,34 @@
+#first
+x = 5
+print(type(x))
+int
+
+#second
+x = "Hello World"
+print(type(x))
+str
+
+#third
+x = 20.5
+print(type(x))
+float
+
+#fourth
+x = ["apple", "banana", "cherry"]
+print(type(x))
+list
+
+#fifth
+x = ("apple", "banana", "cherry")
+print(type(x))
+tuple
+
+#sixth
+x = {"name" : "John", "age" : 36}
+print(type(x))
+dict
+
+#seventh
+x = True
+print(type(x))
+bool
