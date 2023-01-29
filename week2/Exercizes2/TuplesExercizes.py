ba0d6f651fc424a321ed7e9bffa4050cae8cbcae
Ayaulim2023 <ayaulim1002k@gmail.com>
2023-01-29 12:56:02 +0600

    part4

---
+++ week2/Exercizes2/TuplesExercizes.py
@@ -0,0 +1,15 @@
+#first
+fruits = ("apple", "banana", "cherry")
+print(fruits[0])
+
+#second
+fruits = ("apple", "banana", "cherry")
+print(len(fruits))
+
+#third
+fruits = ("apple", "banana", "cherry")
+print(fruits[-1])
+
+#fourth
+fruits = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(fruits[2:5])
