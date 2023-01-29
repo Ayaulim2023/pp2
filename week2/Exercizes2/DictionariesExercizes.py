0a6e79209a5e57317b5c4052f379c26e4bc9712a
Ayaulim2023 <ayaulim1002k@gmail.com>
2023-01-29 13:03:23 +0600

    part6

---
+++ week2/Exercizes2/DictionariesExercizes.py
@@ -0,0 +1,45 @@
+#first
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(car.get("model"))
+
+#second
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car["year"]= 2020
+
+#third
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car["color"] = "red"
+
+#fourth
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car.pop("model")
+
+#fifth
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+car.clear()
+
+
+
+
+
