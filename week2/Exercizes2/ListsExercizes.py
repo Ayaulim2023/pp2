566c13548c400cf212405d9eaa44a7bcb4be5c3a
Ayaulim2023 <ayaulim1002k@gmail.com>
2023-01-29 12:53:27 +0600

    part3

---
+++ week2/Exercizes2/ListsExercizes.py
@@ -0,0 +1,36 @@
+#first
+fruits = ["apple", "banana", "cherry"]
+print(fruits[1])
+
+#second
+fruits = ["apple", "banana", "cherry"]
+fruits[0] = "kiwi"
+
+#third
+fruits = ["apple", "banana", "cherry"]
+fruits.append("orange")
+
+#fourth
+fruits = ["apple", "banana", "cherry"]
+fruits.insert(1, "lemon")
+
+#fifth
+fruits = ["apple", "banana", "cherry"]
+fruits.remove("banana")
+
+#sixth
+fruits = ["apple", "banana", "cherry"]
+print(fruits[-1])
+
+#seventh
+fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(fruits[2:5])
+
+#eighth
+fruits = ["apple", "banana", "cherry"]
+print(len(fruits))
+
+
+
+
+
