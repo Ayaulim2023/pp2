c44bf5456b2b9273733c36c3785284ed489a237f
Ayaulim2023 <ayaulim1002k@gmail.com>
2023-01-29 12:59:29 +0600

    part5

---
+++ week2/Exercizes2/SetsExercizes.py
@@ -0,0 +1,23 @@
+#first
+fruits = {"apple", "banana", "cherry"}
+if "apple" in fruits:
+  print("Yes, apple is a fruit!")
+
+#second
+fruits = {"apple", "banana", "cherry"}
+fruits.add("orange")
+
+#third
+fruits = {"apple", "banana", "cherry"}
+more_fruits = ["orange", "mango", "grapes"]
+fruits.update(more_fruits)
+
+#fourth
+fruits = {"apple", "banana", "cherry"}
+fruits.remove("banana")
+
+#fifth
+fruits = {"apple", "banana", "cherry"}
+fruits.discard("banana")
+
+
