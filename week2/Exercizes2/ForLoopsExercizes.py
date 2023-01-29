7889e9fa23895c2966624d5e8a6a8933969b45e0
Ayaulim2023 <ayaulim1002k@gmail.com>
2023-01-29 12:47:50 +0600

    part2

---
+++ week2/Exercizes2/ForLoopsExercizes.py
@@ -0,0 +1,25 @@
+#first
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+
+  print(x)
+
+#second
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    continue
+
+  print(x)
+
+#third
+for x in range(6):
+  print(x)
+
+#fourth
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+
+  print(x)
