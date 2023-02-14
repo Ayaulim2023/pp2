11ee52333d345edd8cc8f82a1c543053a7bf4934
Ayaulim2023 <ayaulim1002k@gmail.com>
2023-02-14 11:06:05 +0600

    functions1.4

---
+++ week3/Functions1.md/ex4.py
@@ -0,0 +1,12 @@
+def filter_primes(numbers):
+    def is_prime(num):
+        if num <= 1:
+            return False
+        for i in range(2, num):
+            if num % i == 0:
+                return False
+        return True
+    return [num for num in numbers if is_prime(num)]
+a = [int(s) for s in input().split()]
+
+print(filter_primes(a))
