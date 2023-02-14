73f7305ff3d4af0c4495b7002620af9d05d3d74b
Ayaulim2023 <ayaulim1002k@gmail.com>
2023-02-14 11:58:00 +0600

    part1

---
+++ week3/Exercizes/Classes Exercizes/ex1.py
@@ -0,0 +1,26 @@
+#first
+
+class MyClass:
+  x = 5
+
+#second
+class MyClass:
+  x = 5
+
+p1 = MyClass()
+
+#third
+class MyClass:
+  x = 5
+
+p1 = MyClass()
+
+print(p1.x)
+
+#fourth
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
