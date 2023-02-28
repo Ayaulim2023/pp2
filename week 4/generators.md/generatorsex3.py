377d94c2b54700d0ef94d3114b6d925df5550228
Ayaulim2023 <ayaulim1002k@gmail.com>
2023-02-28 16:27:26 +0600

    Define a function with a generator which can iterate the numbers, which are divisible by 3 and 4, between a given range 0 and n

---
+++ week 4/generators.md/generatorsex3.py
@@ -0,0 +1,16 @@
+def EvenGenerator(n):
+    i=0
+    while i<=n:
+        if i%3==0 and i%4==0:
+            yield i
+        i+=1
+
+
+n=int(input())
+values = []
+
+for i in EvenGenerator(n):
+    values.append(str(i))
+i_iter = iter(values)
+for i in EvenGenerator(n):
+  print (next(i_iter))
