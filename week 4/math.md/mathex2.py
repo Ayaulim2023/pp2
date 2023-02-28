accf638312667f54775ad22d863f7710fd097a01
Ayaulim2023 <ayaulim1002k@gmail.com>
2023-02-28 17:10:07 +0600

    Python program to calculate the area of a trapezoid

---
+++ week 4/math.md/mathex2.py
@@ -0,0 +1,8 @@
+def area(h,a,b):
+    return h*(a+b)/2
+
+h = int(input("Height: "))
+a = int(input("Base, first value: "))
+b = int(input("Base, second value: "))
+
+print("Expected Output: ",area(h,a,b))
